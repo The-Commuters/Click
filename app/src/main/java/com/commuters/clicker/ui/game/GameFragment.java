3a978dff8dc1906bad8413a449e6a439a48deb90
Markus F <mafen@users.noreply.github.com>
2019-11-11 19:03:04 +0100

    Update GameFragment.java

--- app/src/main/java/com/commuters/clicker/ui/game/GameFragment.java
+++ app/src/main/java/com/commuters/clicker/ui/game/GameFragment.java
@@ -6,24 +6,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.commuters.clicker.data.entities.Profile;
-import com.commuters.clicker.databinding.FragmentDashboardBinding;
+import com.commuters.clicker.databinding.FragmentGameBinding;
 
 public class GameFragment extends Fragment {
 
-    private GameViewModel gameViewModel;
-
-    FragmentDashboardBinding bindingClicker;
-
-
-
+    FragmentGameBinding bindingClicker;
     Profile localprofile;
+    private GameViewModel gameViewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,28 +29,24 @@ public class GameFragment extends Fragment {
         gameViewModel.getLocalProfile().observe(this, new Observer<Profile>() {
             @Override
             public void onChanged(Profile profile) {
-              localprofile = profile;
-              bindingClicker.setProfile(localprofile);
+                localprofile = profile;
+                bindingClicker.setProfile(localprofile);
 
             }
         });
-
 
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-
         super.onActivityCreated(savedInstanceState);
-
-
-
     }
 
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+
         gameViewModel.update(localprofile);
 
         Log.d("Frag", "I was killed");
@@ -65,28 +57,24 @@ public class GameFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
 
-
-        bindingClicker = FragmentDashboardBinding.inflate(inflater, container, false);
+        bindingClicker = FragmentGameBinding.inflate(inflater, container, false);
 
         View root = bindingClicker.getRoot();
 
 
-  bindingClicker.click.setOnClickListener(new View.OnClickListener() {
+        bindingClicker.click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-
                 update();
-
             }
         });
 
         return root;
     }
 
-    public void update(){
-        localprofile.setScore(localprofile.getScore() +1);
+    public void update() {
+        localprofile.setScore(localprofile.getScore() + localprofile.getClick_strength());
+        gameViewModel.update(localprofile);
     }
 
 }
