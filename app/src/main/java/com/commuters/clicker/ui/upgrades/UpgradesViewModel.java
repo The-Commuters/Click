ccacc2a702c048536c50a47b5affd65ff85ca76a
Markus F <mafen@users.noreply.github.com>
2019-11-11 19:03:53 +0100

    Update UpgradesViewModel.java

--- app/src/main/java/com/commuters/clicker/ui/upgrades/UpgradesViewModel.java
+++ app/src/main/java/com/commuters/clicker/ui/upgrades/UpgradesViewModel.java
@@ -1,19 +1,49 @@
 package com.commuters.clicker.ui.upgrades;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-public class UpgradesViewModel extends ViewModel {
+import com.commuters.clicker.data.entities.Profile;
+import com.commuters.clicker.data.repository.ProfileRepository;
 
-    private MutableLiveData<String> mText;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-    public UpgradesViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+
+public class UpgradesViewModel extends AndroidViewModel {
+
+    private ProfileRepository profileRepository;
+    private LiveData<Profile> localProfile;
+
+
+    public UpgradesViewModel(@NonNull Application application) {
+        super(application);
+        profileRepository = new ProfileRepository(application);
+        localProfile = profileRepository.getLocalProfile();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<Profile> getLocalProfile() {
+        return localProfile;
+    }
+
+    public void update(Profile profile) {
+        profileRepository.updateOnlineProfile(profile.getUsername(), profile, new Callback<Void>(){
+
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+
+            }
+        } );
+
+        profileRepository.updateLocalProfile(profile);
     }
 }
