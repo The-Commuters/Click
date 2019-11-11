82024d36494f5d02b91a117e73465c95787bdf78
Markus F <mafen@users.noreply.github.com>
2019-11-11 18:59:03 +0100

    Added clickStrengthUpgrade

--- app/src/main/java/com/commuters/clicker/data/entities/Profile.java
+++ app/src/main/java/com/commuters/clicker/data/entities/Profile.java
@@ -5,7 +5,6 @@ import androidx.annotation.NonNull;
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
 import androidx.databinding.library.baseAdapters.BR;
-
 import androidx.room.Entity;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
@@ -21,7 +20,6 @@ public class Profile extends BaseObservable {
 
     private String password;
 
-
     private int score;
 
     private int click_strength;
@@ -29,6 +27,21 @@ public class Profile extends BaseObservable {
     private int combo_multiplier;
 
     private int combo_speed;
+
+    // Clicking constants
+    final static int CLICK_STRENGTH_BASE = 1; // hvor mangen poeng som blir tjent per click
+    final static int CLICK_STRENGTH_MAXIMUM = 20; // maximumen til den over
+    final static int COUNTER_MINIMUM = 0;
+
+    // Cost-variables
+    final static int CLICK_STRENGTH_COST_BASE = 10;
+    final static int COMBO_STRENGTH_COST_BASE = 80;
+    final static int COMBO_SPEED_COST_BASE = 200;
+
+    // Constants to calculate the cost of upgrades
+    final static double CLICK_STRENGTH_GROWTH_RATE = 1.2;
+    final static double COMBO_STRENGTH_GROWTH_RATE = 0.5;
+    final static double COMBO_SPEED_GROWTH_RATE = 0.3;
 
     public Profile(
             String username,
@@ -50,6 +63,7 @@ public class Profile extends BaseObservable {
     }
 
     public void setClick_strength(int click_strength) {
+        notifyPropertyChanged(BR.click_strength);
         this.click_strength = click_strength;
     }
 
@@ -79,15 +93,29 @@ public class Profile extends BaseObservable {
         return score;
     }
 
+    @Bindable
     public int getClick_strength() {
         return click_strength;
     }
 
+    @Bindable
     public int getCombo_multiplier() {
         return combo_multiplier;
     }
 
+    @Bindable
     public int getCombo_speed() {
         return combo_speed;
+    }
+
+    public void incrementClickStrength() {
+        this.setClick_strength(this.getClick_strength() + (int) (CLICK_STRENGTH_GROWTH_RATE));
+        this.setScore(this.getScore() - this.getClickStrengthCost());
+    }
+
+    @Bindable
+    public int getClickStrengthCost() {
+        notifyPropertyChanged(BR.clickStrengthCost);
+        return (int) (CLICK_STRENGTH_COST_BASE * (CLICK_STRENGTH_GROWTH_RATE * click_strength));
     }
 }
