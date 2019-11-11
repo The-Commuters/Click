71770c3cbdd1b1d0a1257efb8867de3e0026dfb6
Markus F <mafen@users.noreply.github.com>
2019-11-11 19:01:41 +0100

    Update UpgradesFragment.java

--- app/src/main/java/com/commuters/clicker/ui/upgrades/UpgradesFragment.java
+++ app/src/main/java/com/commuters/clicker/ui/upgrades/UpgradesFragment.java
@@ -1,35 +1,80 @@
 package com.commuters.clicker.ui.upgrades;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 
-import com.commuters.clicker.R;
+import com.commuters.clicker.data.entities.Profile;
+import com.commuters.clicker.databinding.FragmentUpgradesBinding;
 
 public class UpgradesFragment extends Fragment {
 
     private UpgradesViewModel upgradesViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        upgradesViewModel =
-                ViewModelProviders.of(this).get(UpgradesViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        upgradesViewModel.getText().observe(this, new Observer<String>() {
+    FragmentUpgradesBinding bindingUpgrades;
+
+    Profile localprofile;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        upgradesViewModel = new ViewModelProvider(this).get(UpgradesViewModel.class);
+        upgradesViewModel.getLocalProfile().observe(this, new Observer<Profile>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onChanged(Profile profile) {
+                localprofile = profile;
+                bindingUpgrades.setProfile(localprofile);
             }
         });
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        upgradesViewModel.update(localprofile);
+
+        Log.d("Frag", "I was killed");
+
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        bindingUpgrades = FragmentUpgradesBinding.inflate(inflater, container, false);
+
+        View root = bindingUpgrades.getRoot();
+
+        bindingUpgrades.clickUpgradeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                localprofile.incrementClickStrength();
+                upgradesViewModel.update(localprofile);
+            }
+        });
+
         return root;
     }
+
+    public void update() {
+        localprofile.setScore(localprofile.getScore() + 1);
+    }
+
 }
